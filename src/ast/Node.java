b269c671ce8d2143d38459d33a5280d1e1ad91c3
Nemesis <lorenzo.balugani@gmail.com>
2022-03-22 16:00:58 +0100

    :sparkles: Add initial files, project has begun

---
+++ src/ast/Node.java
@@ -0,0 +1,18 @@
+package ast;
+
+import java.util.ArrayList;
+
+import util.Environment;
+import util.SemanticError;
+
+public interface Node {
+
+    String toPrint(String indent);
+
+    Node typeCheck();
+
+    String codeGeneration();
+
+    ArrayList<SemanticError> checkSemantics(Environment env);
+
+}
---
+++ src/util/Environment.java
@@ -0,0 +1,12 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import ast.STentry;
+
+public class Environment {
+    public ArrayList<HashMap<String, STentry>> symTable = new ArrayList<HashMap<String, STentry>>();
+    public int nestingLevel = -1;
+    public int offset = 0;
+}
