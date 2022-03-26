9216d47866ec758a7013cc3bf54cef002fe4e2dd
Alessandra22 <alessandra.cosenza21@gmail.com>
2022-03-26 08:55:08 +0100

    :sparkles: Add AssignmentNode

---
+++ src/ast/AssignmentNode.java
@@ -0,0 +1,38 @@
+package ast;
+
+import ast.ExpNodes.ValExpNode;
+import util.Environment;
+import util.SemanticError;
+
+import java.util.ArrayList;
+
+public class AssignmentNode implements Node{
+    // ID '=' exp
+    private Node ID;
+    private Node exp;
+
+    public AssignmentNode(Node ID, Node exp){
+        this.ID = ID;
+        this.exp = exp;
+    }
+
+    @Override
+    public String toPrint(String indent) {
+        return ID.toPrint(indent) + " = " + exp.toPrint("");
+    }
+
+    @Override
+    public Node typeCheck() {
+        return null;
+    }
+
+    @Override
+    public String codeGeneration() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<SemanticError> checkSemantics(Environment env) {
+        return null;
+    }
+}
