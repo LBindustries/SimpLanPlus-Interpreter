25f407e6f616d74da04050ebda51615f0509df5c
Nemesis <lorenzo.balugani@gmail.com>
2022-03-26 08:42:33 +0100

    :sparkles: Add IteNode

---
+++ src/ast/IteNode.java
@@ -0,0 +1,44 @@
+package ast;
+
+import util.Environment;
+import util.SemanticError;
+
+import java.util.ArrayList;
+
+public class IteNode implements Node{
+
+    private Node exp;
+    private Node then_statement;
+    private Node else_statement;
+
+    public IteNode(Node exp, Node then_statement, Node else_statement){
+        this.exp = exp;
+        this.then_statement = then_statement;
+        this.else_statement = else_statement;
+    }
+
+    public IteNode(Node exp, Node then_statement){
+        this.exp = exp;
+        this.then_statement = then_statement;
+    }
+
+    @Override
+    public String toPrint(String indent) {
+        return null;
+    }
+
+    @Override
+    public Node typeCheck() {
+        return null;
+    }
+
+    @Override
+    public String codeGeneration() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<SemanticError> checkSemantics(Environment env) {
+        return null;
+    }
+}
